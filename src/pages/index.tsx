e610aa10b72aea4d08a964441279653e2cebe3c0
daichitoyohara <daiiiichit@gmail.com>
2025-03-24 22:13:26 +0900

    ページ内に画像の追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Link href="/posts/first-post">First Post</Link>
+      <div className={styles.main}>
+        <Image src="/reysol.png" alt="Reysol Logo" width={180} height={180} />
+        <Link href="/posts/first-post">First Post</Link>
+      </div>
     </>
   );
 }
